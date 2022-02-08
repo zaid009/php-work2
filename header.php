7bfdf64ec7ec5cf1aa1d7c0528ec76bc7546e47b
zaid009 <92687804+zaid009@users.noreply.github.com>
2022-02-08 06:18:28 -0800

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>header</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dongle&family=Rowdies:wght@300&display=swap" rel="stylesheet">
 </head>
+
 <body>
      <!-- Header Section Begin -->
      <header class="header">
@@ -13,11 +17,8 @@
             <div class="row">
                 <div class="col-lg-2">
                     <div class="header__logo">
-                        <a href="./index.html">
-                          <!--<img src="img/logo.png" alt=""> -->
-                          
-                         Hello <?php echo $_SESSION['name'];?>
-                          
+                    <a href="index.php">
+                            <img src="img/logo.png" alt="">
                         </a>
                     </div>
                 </div>
@@ -25,7 +26,7 @@
                     <div class="header__nav">
                         <nav class="header__menu mobile-menu">
                             <ul>
-                                <li class="active"><a href="index.php">Homepage</a></li>
+                                <li class=""><a href="index.php">Homepage</a></li>
                                 <li><a href="./categories.html">Categories <span class="arrow_carrot-down"></span></a>
                                     <ul class="dropdown">
                                         <li><a href="./categories.html">Categories</a></li>
@@ -37,7 +38,7 @@
                                     </ul>
                                 </li>
                                 <li><a href="./blog.html">Our Blog</a></li>
-                                <li><a href="#">Contacts</a></li>
+                                <li><a href="contact.php">Contacts</a></li>
                                 <li><a href="logout.php">Logout</a></li>
                             </ul>
                         </nav>
@@ -47,8 +48,15 @@
                     <div class="header__right">
                         <a href="#" class="search-switch"><span class="icon_search"></span></a>
                         <a href="login.php"><span class="icon_profile">
-                            
+                        
+                          <!--<img src="img/logo.png" alt=""> -->
+                         
+                      
                         </span></a>
+                        <a  style="font-family:'Rowdies', cursive; color:aliceblue">
+                        <span> Hello <?php echo $_SESSION['name'];?></span>
+                          </a>
+
                     </div>
                 </div>
             </div>
